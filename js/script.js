fd3a31a3f0fcb3861a080e0152f8319d4bda91c8
Ilakiab08 <142892200+Ilakiab08@users.noreply.github.com>
2023-12-12 09:36:13 +0100

    base code

---
+++ js/script.js
@@ -0,0 +1,36 @@
+
+document.addEventListener('DOMContentLoaded', function () {
+    const moviesContainer = document.getElementById('movies-container');
+  
+    // Replace with your API endpoint
+    const API_URL = 'YOUR_API_ENDPOINT';
+  
+    // Fetch movies from the API
+    fetch(`${API_URL}/movies`)
+      .then(response => response.json())
+      .then(data => {
+        data.forEach(movie => {
+          const movieElement = createMovieElement(movie);
+          moviesContainer.appendChild(movieElement);
+        });
+      })
+      .catch(error => console.error('Error fetching data:', error));
+  
+    function createMovieElement(movie) {
+      const movieDiv = document.createElement('div');
+      movieDiv.classList.add('movie');
+  
+      const img = document.createElement('img');
+      img.src = movie.poster;
+      img.alt = movie.title;
+  
+      const title = document.createElement('h2');
+      title.textContent = movie.title;
+  
+      movieDiv.appendChild(img);
+      movieDiv.appendChild(title);
+  
+      return movieDiv;
+    }
+  });
+  
