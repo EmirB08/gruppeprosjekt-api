336c3a8bb3619c0fb5a75d751f15029e177585c1
Ilakiab08 <142892200+Ilakiab08@users.noreply.github.com>
2023-12-15 12:05:09 +0100

    search function works

--- js/shows.js
+++ js/shows.js
@@ -1,13 +1,13 @@
-const apiUrl = "https://api.tvmaze.com/shows"; // Im just using the people API, you replace this with whatever you are working on
+const apiUrl = "https://api.tvmaze.com/shows";
 
-const getItems = async (url) => { //async function to get the items from the API
+const getItems = async (url) => {
     const response = await fetch(url);
     const items = await response.json();
-    displayItems(items); // I'm calling the  array of items 'items' instead of 'shows' because the API can return other types of items like movies depending on the URL
+    displayItems(items);
 };
 
-const displayItems = (items) => { //function to display the items
-    const container = document.getElementById("items-container") || createContainer("items-container"); // container to display the items, if it doesn't exist create it using the createContainer function
+const displayItems = (items) => {
+    const container = document.getElementById("items-container") || createContainer("items-container");
     container.innerHTML = "";
 
     items.forEach(item => {
@@ -16,31 +16,31 @@ const displayItems = (items) => { //function to display the items
     });
 };
 
-const createContainer = (id) => { //function to create a container with the given id
+const createContainer = (id) => {
     const container = document.createElement("div");
     container.id = id;
     document.body.appendChild(container);
     return container;
 };
 
-const createItemCard = (item) => { //function to create a card for the given item, reusable for other types of items
+const createItemCard = (item) => {
     const card = document.createElement("div");
     card.className = "item-card";
 
-    const image = document.createElement("img"); // Create an image element
+    const image = document.createElement("img");
     image.src = item.image ? item.image.medium : "placeholder.jpg";
     image.alt = item.name || item.title || "Image";
     image.className = "item-image";
     card.appendChild(image);
 
-    if (item.name || item.title) { // If the item has a name or title, create a title element
+    if (item.name || item.title) {
         const title = document.createElement("h4");
         title.textContent = item.name || item.title;
         title.className = "item-title";
         card.appendChild(title);
     }
 
-    if (item.rating && item.rating.average) { // If the item has a rating, create a rating element
+    if (item.rating && item.rating.average) {
         const rating = document.createElement("p");
         rating.textContent = `Rating: ${item.rating.average}`;
         rating.className = "item-rating";
@@ -49,4 +49,44 @@ const createItemCard = (item) => { //function to create a card for the given ite
     return card;
 };
 
+const createSearchElements = () => {
+    const searchContainer = document.createElement("div");
+    searchContainer.className = "search-container";
+
+    const searchInput = document.createElement("input");
+    searchInput.id = "search-input";
+    searchInput.placeholder = "Search Shows";
+    searchInput.className = "search-input";
+
+    const searchButton = document.createElement("button");
+    searchButton.textContent = "Search";
+    searchButton.className = "search-button";
+    searchButton.addEventListener("click", () => performSearch(searchInput.value));
+
+    searchContainer.appendChild(searchInput);
+    searchContainer.appendChild(searchButton);
+    document.body.appendChild(searchContainer);
+};
+
+const performSearch = async (query) => {
+    query = query.toLowerCase();
+    const container = document.getElementById("items-container");
+    container.innerHTML = ''; // Clear existing items
+
+    try {
+        const response = await fetch(`https://api.tvmaze.com/search/shows?q=${query}`);
+        if (!response.ok) {
+            throw new Error(`Failed to fetch search results. Status: ${response.status}`);
+        }
+
+        const searchResults = await response.json();
+        displayItems(searchResults.map(result => result.show));
+    } catch (error) {
+        console.error('Error performing search:', error);
+        // Display an error message to the user if needed
+    }
+};
+
+// Create search elements and fetch shows when the page loads
+createSearchElements();
 getItems(apiUrl);
