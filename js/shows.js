24199b2b3cc7328f39516ba68da2cc547944ac27
Ilakiab08 <142892200+Ilakiab08@users.noreply.github.com>
2023-12-15 10:16:35 +0100

    search

--- js/shows.js
+++ js/shows.js
@@ -1,74 +1,70 @@
-document.addEventListener("DOMContentLoaded", function () {
-  const apiUrl = "https://api.tvmaze.com/shows";
+const apiUrl = "https://api.tvmaze.com/shows?";// Im just using the people API, you replace this with whatever you are working on
+const searchShows = async () => {
+  const searchInput = document.getElementById("search-input").value.toLowerCase();
+  const searchUrl = `${apiUrl}/search/shows?q=${encodeURIComponent(searchInput)}`;
+  console.log("Search URL:", searchUrl); // Log the constructed URL
+  getItems(searchUrl);
+};
 
-  const searchShows = async () => {
-    const searchInput = document.getElementById("search-input").value.toLowerCase();
-    const searchUrl = `${apiUrl}/search/shows?q=${encodeURIComponent(searchInput)}`;
-    console.log("Search URL:", searchUrl);
-    getItems(searchUrl);
+const getItems = async (url) => {
+    const response = await fetch(url);
+    const shows = await response.json(); // Rename 'items' to 'shows' for clarity
+    displayItems(shows);
   };
 
-  const getItems = async (url) => {
-    try {
-      const response = await fetch(url);
-      const shows = await response.json();
-      displayItems(shows);
-    } catch (error) {
-      console.error("Error fetching data:", error);
-    }
-  };
 
-  const displayItems = (items) => {
-    const container =
-      document.getElementById("items-container") ||
-      createContainer("items-container");
-    container.innerHTML = "";
+const displayItems = (items) => {
+  //function to display the items
+  const container =
+    document.getElementById("items-container") ||
+    createContainer("items-container"); // container to display the items, if it doesn't exist create it using the createContainer function
+  container.innerHTML = "";
 
-    items.forEach((item) => {
-      const card = createItemCard(item);
-      container.appendChild(card);
-    });
-  };
+  items.forEach((item) => {
+    const card = createItemCard(item);
+    container.appendChild(card);
+  });
+};
 
-  const createContainer = (id) => {
-    const container = document.createElement("div");
-    container.id = id;
-    document.body.appendChild(container);
-    return container;
-  };
+const createContainer = (id) => {
+  //function to create a container with the given id
+  const container = document.createElement("div");
+  container.id = id;
+  document.body.appendChild(container);
+  return container;
+};
 
-  const createItemCard = (item) => {
-    const card = document.createElement("div");
-    card.className = "item-card";
+const createItemCard = (item) => {
+  //function to create a card for the given item, reusable for other types of items
+  const card = document.createElement("div");
+  card.className = "item-card";
 
-    const image = document.createElement("img");
-    image.src = item.image ? item.image.medium : "placeholder.jpg";
-    image.alt = item.name || item.title || "Image";
-    image.className = "item-image";
-    card.appendChild(image);
+  const image = document.createElement("img"); // Create an image element
+  image.src = item.image ? item.image.medium : "placeholder.jpg";
+  image.alt = item.name || item.title || "Image";
+  image.className = "item-image";
+  card.appendChild(image);
 
-    if (item.name || item.title) {
-      const title = document.createElement("h4");
-      title.textContent = item.name || item.title;
-      title.className = "item-title";
-      card.appendChild(title);
-    }
+  if (item.name || item.title) {
+    // If the item has a name or title, create a title element
+    const title = document.createElement("h4");
+    title.textContent = item.name || item.title;
+    title.className = "item-title";
+    card.appendChild(title);
+  }
 
-    if (item.rating && item.rating.average) {
-      const rating = document.createElement("p");
-      rating.textContent = `Rating: ${item.rating.average}`;
-      rating.className = "item-rating";
-      card.appendChild(rating);
-    }
-    return card;
-  };
+  if (item.rating && item.rating.average) {
+    // If the item has a rating, create a rating element
+    const rating = document.createElement("p");
+    rating.textContent = `Rating: ${item.rating.average}`;
+    rating.className = "item-rating";
+    card.appendChild(rating);
+  }
+  return card;
+};
 
-  // Event listener for the search input
-  const searchInput = document.getElementById("search-input");
-  console.log("Search input element:", searchInput);
+// Event listener for the search input
+const searchInput = document.getElementById("search-input");
+searchInput.addEventListener("input", searchShows);
 
-  searchInput.addEventListener("input", searchShows);
-  console.log("Event listener registered for search input.");
-
-  getItems(apiUrl);
-});
+getItems(apiUrl);
