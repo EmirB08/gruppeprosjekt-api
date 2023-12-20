96297a2ede98d3beb2e3b1596db6b797e2befb99
André <137167063+AndreK-B06@users.noreply.github.com>
2023-12-20 10:26:51 +0100

    oppdate fom dev

--- js/people.js
+++ js/people.js
@@ -1,11 +1,11 @@
-const apiUrl = "https://api.tvmaze.com/people"; // Im just using the people API, you replace this with whatever you are working on
-const getItems = async (url, page = 1, pageSize = 20) => {
-  const response = await fetch(`${url}?page=${page}&size=${pageSize}`);
-  const items = await response.json();
-  console.log(items);
-
-  displayItems(items, page, pageSize); // I'm calling the  array of items 'items' instead of 'shows' because the API can return other types of items like movies depending on the URL
-};
+import {
+  createContainer,
+  createItemCard,
+  displayShowDetails,
+  createSearchElements,
+  toggleFavorite,
+  manageFavorites,
+} from "./uiComponents.js";
 
 const displayItems = (items, page, pageSize) => {
   //function to display the items
@@ -17,53 +17,17 @@ const displayItems = (items, page, pageSize) => {
   const startIndex = (page - 1) * pageSize;
   const endIndex = startIndex + pageSize;
   const itemsToShow = items.slice(startIndex, endIndex);
-  itemsToShow.forEach((item) => {
+  items.forEach((item) => {
     const card = createItemCard(item);
     container.appendChild(card);
   });
-};
-
-const createContainer = (id) => {
-  //function to create a container with the given id
-  const container = document.createElement("div");
-  container.id = id;
-  document.body.appendChild(container);
-  return container;
-};
-
-const createItemCard = (item) => {
-  //function to create a card for the given item, reusable for other types of items
-  const card = document.createElement("div");
-  card.className = "item-card";
-
-  const image = document.createElement("img"); // Create an image element
-  image.src = item.image ? item.image.medium : "placeholder.jpg";
-  image.alt = item.name || item.title || "Image";
-  image.className = "item-image";
-  card.appendChild(image);
-
-  if (item.name || item.title) {
-    // If the item has a name or title, create a title element
-    const title = document.createElement("h4");
-    title.textContent = item.name || item.title;
-    title.className = "item-title";
-    card.appendChild(title);
-  }
-
-  if (item.rating && item.rating.average) {
-    // If the item has a rating, create a rating element
-    const rating = document.createElement("p");
-    rating.textContent = `Rating: ${item.rating.average}`;
-    rating.className = "item-rating";
-    card.appendChild(rating);
-  }
-  return card;
 };
 /* ------------
 !!! Search !!!
 ------------- */
 // Function to perform the search
 const performSearch = async (query, page = 1, pageSize = 20) => {
+  // this is very intense on the API, will need to be changed Andre
   if (query.trim() === "") {
     getItems(apiUrl, page, pageSize);
   } else {
@@ -105,16 +69,13 @@ const createPaginationButton = (text, id, clickHandler) => {
 // Set the default pageSize
 let pageSize = 20;
 // Function to handle pagination
-const handlePagination = () => {
-  getItems(apiUrl, currentPage, pageSize);
+const handlePagination = (pageSize) => {
+  performSearch(searchInput.value.toLowerCase(), pageSize);
 };
 
 // Function to update current page and perform search
 const updatePageAndSearch = (increment) => {
   currentPage += increment;
-  if (currentPage < 1) {
-    currentPage = 1;
-  }
   handlePagination();
   console.log(currentPage);
 };
