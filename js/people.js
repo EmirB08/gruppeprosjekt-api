725f42a61cca1028f49f00c6da568d14ed95b8be
André <137167063+AndreK-B06@users.noreply.github.com>
2023-12-20 09:22:31 +0100

    inplementet code from dev

--- js/people.js
+++ js/people.js
@@ -1,13 +1,9 @@
-import {createContainer, createItemCard, displayShowDetails, createSearchElements, toggleFavorite, manageFavorites } from './uiComponents.js';
-
 const apiUrl = "https://api.tvmaze.com/people"; // Im just using the people API, you replace this with whatever you are working on
 const getItems = async (url, page = 1, pageSize = 20) => {
   const response = await fetch(`${url}?page=${page}&size=${pageSize}`);
   const items = await response.json();
   console.log(items);
-  const startIndex = (page - 1) * pageSize;
-  const endIndex = startIndex + pageSize;
-  const itemsToShow = items.slice(startIndex, endIndex);
+
   displayItems(items, page, pageSize); // I'm calling the  array of items 'items' instead of 'shows' because the API can return other types of items like movies depending on the URL
 };
 
@@ -18,20 +14,56 @@ const displayItems = (items, page, pageSize) => {
     createContainer("items-container"); // container to display the items, if it doesn't exist create it using the createContainer function
   container.innerHTML = "";
 
-  // const startIndex = (page - 1) * pageSize;
-  // const endIndex = startIndex + pageSize;
-  // const itemsToShow = items.slice(startIndex, endIndex);
-  items.forEach((item) => {
+  const startIndex = (page - 1) * pageSize;
+  const endIndex = startIndex + pageSize;
+  const itemsToShow = items.slice(startIndex, endIndex);
+  itemsToShow.forEach((item) => {
     const card = createItemCard(item);
     container.appendChild(card);
   });
 };
 
+const createContainer = (id) => {
+  //function to create a container with the given id
+  const container = document.createElement("div");
+  container.id = id;
+  document.body.appendChild(container);
+  return container;
+};
+
+const createItemCard = (item) => {
+  //function to create a card for the given item, reusable for other types of items
+  const card = document.createElement("div");
+  card.className = "item-card";
+
+  const image = document.createElement("img"); // Create an image element
+  image.src = item.image ? item.image.medium : "placeholder.jpg";
+  image.alt = item.name || item.title || "Image";
+  image.className = "item-image";
+  card.appendChild(image);
+
+  if (item.name || item.title) {
+    // If the item has a name or title, create a title element
+    const title = document.createElement("h4");
+    title.textContent = item.name || item.title;
+    title.className = "item-title";
+    card.appendChild(title);
+  }
+
+  if (item.rating && item.rating.average) {
+    // If the item has a rating, create a rating element
+    const rating = document.createElement("p");
+    rating.textContent = `Rating: ${item.rating.average}`;
+    rating.className = "item-rating";
+    card.appendChild(rating);
+  }
+  return card;
+};
 /* ------------
 !!! Search !!!
 ------------- */
 // Function to perform the search
-const performSearch = async (query, page = 1, pageSize = 20) => {  // this is very intense on the API, will need to be changed Andre
+const performSearch = async (query, page = 1, pageSize = 20) => {
   if (query.trim() === "") {
     getItems(apiUrl, page, pageSize);
   } else {
