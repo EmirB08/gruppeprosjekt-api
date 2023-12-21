682b7a98ac2901b5073dd4a7732ee55cf4db3d89
EmirB08 <142892269+EmirB08@users.noreply.github.com>
2023-12-21 02:11:03 +0100

    success - shows.js

--- js/shows.js
+++ js/shows.js
@@ -1,76 +1,44 @@
-import { createContainer, createItemCard, createSearchElements, displayShowDetails, toggleFavorite, manageFavorites } from "./uiComponents.js";
+import { getItems, displayItems, createPages, createContainer, createItemCard, displayShowDetails, toggleFavorite, manageFavorites } from './uiComponents.js';
 
-const apiUrl = "https://api.tvmaze.com/shows";
-const itemsPerPage = 18; // Number of items to display per page
+const showsAPI = 'https://api.tvmaze.com/shows';
 
-let currentPage = 1;
-let items = []; // Store items globally
 
-const getItems = async (url, page) => {
-    const response = await fetch(`${url}?page=${page}`);
-    items = await response.json();
-    console.log(items);
-    currentPage = 1; // Update the global items variable
-    displayItems();
+const createSearchElements = () => { //function to create the search elements and search functionality - will change to dual search functionality later
+  const searchContainer = document.createElement("div");
+  searchContainer.className = "search-container";
+
+  const searchInput = document.createElement("input");
+  searchInput.id = "searchInput";
+  searchInput.placeholder = "Search Shows";
+  searchInput.className = "search-input";
+
+  const searchButton = document.createElement("button");
+  searchButton.textContent = "Search";
+  searchButton.className = "search-button";
+  searchButton.addEventListener("click", () => performSearch(searchInput.value)); // will include enter key functionality later
+
+  searchContainer.appendChild(searchInput);
+  searchContainer.appendChild(searchButton);
+  document.body.appendChild(searchContainer); // appending everything
 };
 
-const displayItems = () => {
-    const container = document.getElementById("items-container") || createContainer("items-container");
-    container.innerHTML = "";
-
-    const startIndex = (currentPage - 1) * itemsPerPage;
-    const endIndex = startIndex + itemsPerPage;
-    const itemsToDisplay = items.slice(startIndex, endIndex);
-
-    itemsToDisplay.forEach(item => {
-        const card = createItemCard(item);
-        container.appendChild(card);
-    });
-
-    createPagination();
-};
-
-const createPagination = () => { // some error in your function Illakia, must fix, keeping it like it is for now
-    const paginationContainer = document.getElementById("pagination-container") || createContainer("pagination-container");
-    paginationContainer.innerHTML = "";
-
-    const totalPages = Math.ceil(items.length / itemsPerPage);
-
-  // Previous Button
-    const prevButton = document.createElement("button");
-    prevButton.textContent = "Previous";
-    prevButton.addEventListener("click", () => {
-        if (currentPage > 1) {
-        currentPage--;
-        displayItems();
-    }
-    });
-    paginationContainer.appendChild(prevButton);
-
-  // Next Button
-    const nextButton = document.createElement("button");
-    nextButton.textContent = "Next";
-    nextButton.addEventListener("click", async () => {
-    if (currentPage < totalPages) {
-        currentPage++;
-        displayItems();
-    } else {
-        currentPage++;
-        await getItems(apiUrl, currentPage);
-    }
-});
-paginationContainer.appendChild(nextButton);
+const performSearch = async (query) => { //takes in the query from the search input 
+  const response = await fetch(`https://api.tvmaze.com/search/shows?q=${query}`); //using the query to search the API search endpoint
+  const searchResults = await response.json();
+  console.log(searchResults); //
+  displayItems(searchResults.map(result => result.person)); //array map the search result, very similar to the displayItems function here
+  window.history.pushState({ searchQuery: query }, '', `?search=${encodeURIComponent(query)}`); //update the URL and history state - very neat
 };
 
 window.onpopstate = (event) => { // IMPORTANT: NEEDS TO BE EDITED DEPENDING ON THE HTML - FOR FUTURE REFERENCE
-    if (event.state && event.state.show) { // If there's a show in the history state, display it
-            displayShowDetails(event.state.show);
-    } else {
-        const container = document.getElementById("items-container"); //clear the cointainer and display the "default page"
-        container.innerHTML = '';
-        getItems(apiUrl,currentPage); //only need to change this to the "default" page of shows.js - will fix later for search query logic so that it doesn't display the default page when there's a search in "queue"
-    }
+  if (event.state && event.state.content) { // If there's a show in the history state, display it
+          displayShowDetails(event.state.content);
+  } else {
+      const container = document.getElementById("items-container"); //clear the cointainer and display the "default page"
+      container.innerHTML = '';
+      createPages(showsAPI); //only need to change this to the "default" page of shows.js - will fix later for search query logic so that it doesn't display the default page when there's a search in "queue"
+  }
 };
 
+createPages(showsAPI);
 createSearchElements();
-getItems(apiUrl, currentPage);
